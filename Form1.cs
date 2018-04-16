b8d97015660156ddcc8c71da6d827bf266b572b9
Jason Borum <33642393+jborum@users.noreply.github.com>
2018-04-15 18:02:12 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * Inventory Form
+ * This program was written by Jason Borum
+ * Date: April 1, 2018
+ * Course: CST117
+ * Instructor: Dr. Brandon Bass
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory
+{
+    public partial class frmInventory : Form
+    {
+        public frmInventory()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            //Initialize variables
+            string category;
+            string name;
+            string number;
+            decimal price;
+
+            // Get variable from form
+            category = txtCategory.Text;
+            name = txtName.Text;
+            number = txtNumber.Text;
+            price = Convert.ToDecimal(txtPrice.Text);
+            Inventory item1 = new Inventory(category, name, number, price);
+            MessageBox.Show(string.Format("The new item added is {0}, {1}, {2}, {3} and there are a total of {4} items", item1.Category, item1.Name, item1.Number, item1.Price, item1.Quantity));
+
+        }
+
+        private void btnDriver_Click(object sender, EventArgs e)
+        {
+            // Create inventory objects
+            Inventory item1 = new Inventory("Toy", "Water Gun", "2018-0971-WaterGun", 24.99m);
+            Inventory item2 = new Inventory("Toy", "Teddy Bear", "2018-8802-Teddy", 29.99m);
+            Inventory item3 = new Inventory("Toy", "Beach Ball", "2018-501-BeachBall", 2.99m);
+            Inventory item4 = new Inventory("Clothing", "Blue Jeans", "2018-091-BlueJeans", 59.99m);
+            Inventory item5 = new Inventory("Shoes", "BasketBall Shoe", "2018-24-BballShoe", 212.99m);
+            Inventory item6 = new Inventory("Shoes", "Football Cleat", "2018-04-Cleat", 74.99m);
+            Inventory item7 = new Inventory("Furniture", "Love Seat", "2018-07-LoveSeat", 984.99m);
+
+            //Initilize inventory manager object
+            InventoryManager cornerStore = new InventoryManager();
+
+            //Add items objects to inventor manager class
+            cornerStore.addItem(item1);
+            cornerStore.addItem(item2);
+            cornerStore.addItem(item3);
+            cornerStore.addItem(item4);
+            cornerStore.addItem(item5);
+            cornerStore.addItem(item6);
+            cornerStore.addItem(item7);
+
+            //Display items after they were added
+            Console.WriteLine("*****Display objects before items are removed*****");
+            cornerStore.displayInventory();
+
+            //Remove items from the inventor manager
+            cornerStore.removeItem(item5);
+            cornerStore.removeItem(item6);
+            cornerStore.removeItem(item7);
+
+            // Display items after some items were removed
+            Console.WriteLine(" ");
+            Console.WriteLine("*****Display objects after some items were removed*****");
+            cornerStore.displayInventory();
+
+            //Restock items method
+            cornerStore.restockItem(item5, 5);
+
+            // Display items after some items were restocked
+            Console.WriteLine(" ");
+            Console.WriteLine("*****Display objects after some items were restocked*****");
+            cornerStore.displayInventory();
+        }
+    }
+}
---
+++ InventoryManager.cs
@@ -0,0 +1,102 @@
+﻿/*
+ * Inventory Manager class
+ * This program was written by Jason Borum
+ * Date: April 15, 2018
+ * Course: CST117
+ * Instructor: Dr. Brandon Bass
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory
+{
+    class InventoryManager
+    {
+        // Initilize inventoryManagerList
+        private List<Inventory> inventoryManagerList = new List<Inventory>();
+
+        // Start additem method
+        public void addItem(Inventory item)
+        {
+            this.inventoryManagerList.Add(item);
+        } // end of add item method
+
+        //Start removeItem method
+        public void removeItem(Inventory item)
+        {
+            this.inventoryManagerList.Remove(item);
+        } // end of remove item method
+
+        // starte restockItem method
+        public void restockItem(Inventory item, int quantity)
+        {
+            int index = 0;
+            // if quantity is greater than 0 then enter the loop
+            if (quantity > 0)
+            {
+                // start while loop to add the number of items determined by the quantity
+                while (index < quantity)
+                {
+                    this.inventoryManagerList.Add(item);
+                    index++;
+                } // end of while loop
+            } // end of iff statement
+        } // End of restock item method
+
+        public void displayInventory()
+        {
+            //Cycle through each product in the list
+            foreach(Inventory product in this.inventoryManagerList)
+            {
+                Console.WriteLine("Catergory = " + product.Category + ", Name = " + product.Name + ", Price = " + product.Price);
+
+            } // end of foreach statement
+        } //end of display Inventory Method
+
+        //Method to search for by item name
+        public List<int> searchName(string objectName)
+        {
+            //Declare list to store positions
+            List<int> itemPosition = new List<int>();
+
+            //cycle through each item in the inventory manager list
+            foreach(Inventory item in this.inventoryManagerList)
+            {
+                //test to see if strings are equal and ignore case
+                if (String.Equals(item.Name, objectName, StringComparison.OrdinalIgnoreCase))
+                {
+                    //if strings are equal add the item position to the list
+                    itemPosition.Add(this.inventoryManagerList.IndexOf(item));
+                } //end of if
+            } // end of foreach
+            return itemPosition;
+        } // End of search name
+
+        //Method to search by item price
+        public List<int> searchPrice(decimal objectPrice)
+        {
+            //Declare list to store positions
+            List<int> itemPosition = new List<int>();
+
+            //cycle through each item in the inventory manager list
+            foreach (Inventory item in this.inventoryManagerList)
+            {
+                //Test if prices match
+                if (item.Price == objectPrice)
+                {
+                    //if prices match then add item position to list
+                    itemPosition.Add(this.inventoryManagerList.IndexOf(item));
+                } // end of if
+            } // end of foreach statement
+
+            //Return list of item positions
+            return itemPosition;
+        } // End of search name
+
+    } // End of InventoryManager Class
+} // End of namespace
